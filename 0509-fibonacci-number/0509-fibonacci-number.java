977f697e7f8a84b9ce51a6530a7bd125d13207ae
Shrey Mewara <hellogit007@gmail.com>
2026-08-30 22:47:13 +0530

    Time: 9 ms (42.85%), Space: 42.1 MB (52.52%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,10 @@
 class Solution {
-    public static int f=0;
-     public static int fibo(int n){
+    public static int fibo(int n){
         if(n==0) return 0;
         if(n==1) return 1;
-        f= fibo(n-1)+fibo(n-2);
-     return f;
-
-     }
+        return fibo(n-1)+fibo(n-2);
+    }
     public int fib(int n) {
-       return fibo(n);
+      return   fibo(n);
     }
 }
