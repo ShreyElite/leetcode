d12f69516ee5d59baf363aa3ffa573fd9abe1c93
Shrey Mewara <hellogit007@gmail.com>
2026-08-31 23:47:28 +0530

    Time: 1 ms (99.98%), Space: 42.9 MB (11.29%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        long n = 0;
+
+        while (x != 0) {
+            n *= 10;
+            n += x % 10;
+            x = x / 10;
+        }
+
+        if (n > Integer.MAX_VALUE || n < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        return (int) n;
+    }
+}
