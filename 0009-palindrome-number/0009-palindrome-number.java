b70da66375a08d001b7f9598ef232f844f75b07b
Shrey Mewara <hellogit007@gmail.com>
2026-09-06 19:21:00 +0530

    Time: 6 ms (21.43%), Space: 46.2 MB (16.13%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+      ArrayList<Integer> list=new ArrayList<>();
+      while(x!=0){
+        list.add(x%10);
+        x=x/10;
+      }
+      int i=0,j=list.size()-1;
+      while(i<=j){
+        if(list.get(i)==list.get(j)){
+            i++;
+            j--;
+        }
+       else {
+        return false;
+       }
+
+      }
+      return true;
+
+    }
+}
